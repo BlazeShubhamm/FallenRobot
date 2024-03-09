1054defe2e5fb819fd524616cd3da8e3cf9925fc
BlazeShubhamm <162813401+BlazeShubhamm@users.noreply.github.com>
2024-03-09 23:24:36 +0530

    Update config.py
    Signed-off-by: BlazeShubhamm <162813401+BlazeShubhamm@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,25 +2,25 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    API_ID = 22347692
+    API_HASH = "f6104693b63671c0fa92343ca8e22183"
 
-    CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
+    CASH_API_KEY = "QWY7Y3V1DCLJSFP7"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://zjnkdsga:nsbtJ01ry62-bmkAKhT7RkZbkwuuhxNy@rain.db.elephantsql.com/zjnkdsga"  # A sql database url from elephantsql.com
 
-    EVENT_LOGS = ()  # Event logs channel to note down important bot level events
+    EVENT_LOGS = (-1002050018620)  # Event logs channel to note down important bot level events
 
-    MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
+    MONGO_DB_URI = "mongodb+srv://blazeshubham:shubham@cluster0.vftwypv.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
 
     SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "6740360553:AAHVJz-WmRfz6LvNxAoYpdI1xO4NvM97d7M"  # Get bot token from @BotFather on Telegram
 
-    TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
+    TIME_API_KEY = "B8BMO8EJAM51"  # Get this value from https://timezonedb.com/api
 
     OWNER_ID = 1356469075  # User id of your telegram account (Must be integer)
 
